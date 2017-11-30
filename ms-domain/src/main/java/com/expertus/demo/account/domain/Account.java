87da998ca2dd0681bece9cbda7cb1c1e4eaf35bd
Bencheikh Mohamed Reda <MahamedReda.Bencheikh@ext.saint-gobain.com>
2017-12-01 00:20:15 +0100

    add resource for account + lombook

--- ms-domain/src/main/java/com/expertus/demo/account/domain/Account.java
+++ ms-domain/src/main/java/com/expertus/demo/account/domain/Account.java
@@ -1,14 +1,16 @@
 package com.expertus.demo.account.domain;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Account {
 
     private Integer id;
     private Integer customerId;
-    private Integer count;
+    private String count;
 
 
 }
