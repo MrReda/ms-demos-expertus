ab5c62ca7b5562cf55a2f09b79394ea31001e92d
Bencheikh Mohamed Reda <MahamedReda.Bencheikh@ext.saint-gobain.com>
2017-11-30 23:03:32 +0100

    AccountDto attributes

--- ms-domain/src/main/java/com/expertus/demo/account/dto/AccountDto.java
+++ ms-domain/src/main/java/com/expertus/demo/account/dto/AccountDto.java
@@ -1,9 +1,14 @@
 package com.expertus.demo.account.dto;
 
+
+import lombok.Data;
+
+@Data
 public class AccountDto {
 
-
-
+    private Integer id;
+    private Integer customerId;
+    private Integer count;
 
 
 }
