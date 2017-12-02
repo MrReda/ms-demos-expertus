ec18afa36c06fe7dd59015d1428e2506be4d5aba
Bencheikh Mohamed Reda <MahamedReda.Bencheikh@ext.saint-gobain.com>
2017-12-02 21:59:06 +0100

    Refactor ms account + adding ms customer

---
+++ ms-account/ms-account-query/src/main/java/com/expertus/demo/account/query/api/AccountQueryResource.java
@@ -0,0 +1,57 @@
+package com.expertus.demo.account.query.api;
+
+
+import com.expertus.demo.account.domain.Account;
+import com.google.common.collect.Lists;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+/**
+ * @author reda
+ */
+
+@RestController
+@RequestMapping("/api")
+public class AccountQueryResource {
+
+
+    private List<Account> accounts;
+
+    AccountQueryResource() {
+        accounts = Lists.newArrayList();
+        accounts.add(new Account(1, 1, "1"));
+        accounts.add(new Account(2, 2, "2"));
+        accounts.add(new Account(3, 3, "3"));
+        accounts.add(new Account(4, 4, "4"));
+        accounts.add(new Account(5, 1, "5"));
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE,path = "/account/{count}")
+    Optional<Account> getByCount(@PathVariable("count") String count) {
+        return accounts.stream().filter(
+                account -> account.getCount().equalsIgnoreCase(count)).findFirst();
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE,path = "/account/customer/{customerId}")
+    Optional<List<Account>> getByCustomer(@PathVariable("customerId") Integer customerId) {
+        List<Account> accountsList = accounts.stream().filter(account -> account.getCustomerId().intValue() == customerId.intValue()).collect(Collectors.toList());
+        return CollectionUtils.isEmpty(accountsList) ? Optional.empty() : Optional.of(accountsList);
+    }
+
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE,path = "/account/all")
+    Optional<List<Account>> getAllAccounts(){
+        return Optional.of(accounts);
+    }
+
+
+}
