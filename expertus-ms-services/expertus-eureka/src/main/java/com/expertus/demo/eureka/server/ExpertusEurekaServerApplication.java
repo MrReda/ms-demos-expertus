03ac0a83ce1219e9df2ba015f5e450560f5419a6
Bencheikh Mohamed Reda <MahamedReda.Bencheikh@ext.saint-gobain.com>
2017-12-07 04:00:26 +0100

    Enable eureka server

--- expertus-ms-services/expertus-eureka/src/main/java/com/expertus/demo/eureka/server/ExpertusEurekaServerApplication.java
+++ expertus-ms-services/expertus-eureka/src/main/java/com/expertus/demo/eureka/server/ExpertusEurekaServerApplication.java
@@ -1,15 +1,16 @@
 package com.expertus.demo.eureka.server;
 
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class ExpertusEurekaServerApplication {
 
-    public static void main(String[] args){
-        SpringApplication.run(ExpertusEurekaServerApplication.class,args);
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertusEurekaServerApplication.class, args);
     }
 
 }
